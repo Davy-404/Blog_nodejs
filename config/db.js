f50d4cb279c337a95a7498694a2bb4f33f96d3f9
Davy-404 <80966301+Davy-404@users.noreply.github.com>
2022-03-17 01:49:13 -0300

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://davy:<password>@blogapp.eojy2.mongodb.net/database?retryWrites=true&w=majority'}
-}else{
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-}
